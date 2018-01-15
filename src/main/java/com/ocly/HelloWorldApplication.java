e0a911b69925894cbcd83e5c19d106a3409069cb
yy1193889747 <1193889747@qq.com>
2018-01-15 14:15:30 +0800

    Initial commit

---
+++ src/main/java/com/ocly/HelloWorldApplication.java
@@ -0,0 +1,20 @@
+package com.ocly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+	@GetMapping("/")
+	public String  hello (){
+		return "Welcome jenkins";
+	}
+}
