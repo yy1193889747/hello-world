a4b98b3f34b04d7afb9ddf1dedf78c35f17a291e
yy1193889747 <1193889747@qq.com>
2018-01-18 18:41:49 +0800

    nginx

--- src/main/java/com/ocly/HelloWorldApplication.java
+++ src/main/java/com/ocly/HelloWorldApplication.java
@@ -2,19 +2,36 @@ package com.ocly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+@Controller
 @SpringBootApplication
-public class HelloWorldApplication {
+@EnableRedisHttpSession
+public class HelloWorldApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
 
-		SpringApplication.run(HelloWorldApplication.class, args);
-	}
-	@GetMapping("/")
-	public String  hello (){
-		return "Welcome jenkins ,I love you,aaa";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HelloWorldApplication.class);
+    }
+
+    @GetMapping("/")
+    public String hello(HttpServletRequest request, Model model) {
+        model.addAttribute("uuid", request.getSession().getId());
+        return "index";
+    }
 }
